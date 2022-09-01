94919f7d83f99d372ad2ab631491eaa680b0b935
tedw87 <teodorvoicu87@gmail.com>
2022-09-01 15:49:11 +0300

    Feat: add function to search by checkin, checkout, country as  query params

--- controllers/propertyToRentController.js
+++ controllers/propertyToRentController.js
@@ -1,26 +1,9 @@
 "use strict";
 
+const moment = require("moment");
 const firebase = require("../db");
 const PropertyToRent = require("../models/propertyToRent");
 const firestore = firebase.firestore();
-
-const addPropertyToRent = async (req, res, next) => {
-  try {
-    const data = req.body;
-    const property = {
-      ...data,
-      id: firestore.collection("propertiesToRent").doc().id,
-    };
-    await firestore
-      .collection("propertiesToRent")
-      .doc(property.id)
-      .set(property);
-    // const id = req.params.id;
-    res.status(201).json(property);
-  } catch (error) {
-    res.status(400).send(error.message);
-  }
-};
 
 const getAllPropertiesToRent = async (req, res, next) => {
   try {
@@ -56,26 +39,20 @@ const getAllPropertiesToRent = async (req, res, next) => {
   }
 };
 
-const getPropertyByCountry = async (req, res, next) => {
+const addPropertyToRent = async (req, res, next) => {
   try {
-    const { country } = req.params;
-    const propertiesCollection = firestore.collection("propertiesToRent");
-    const propertySnapshot = await propertiesCollection
-      .where("location.country", "==", country)
-      .get();
-
-    if (propertySnapshot.empty) {
-      res.status(404).send("Property with the given country not found !");
-    } else {
-      let propertyByCountry = [];
-      propertySnapshot.forEach((doc) => {
-        propertyByCountry.push({ ...doc.data(), id: doc.id });
-      });
-      console.log("property from db:", propertyByCountry);
-      res.send(propertyByCountry);
-    }
+    const data = req.body;
+    const property = {
+      ...data,
+      id: firestore.collection("propertiesToRent").doc().id,
+    };
+    await firestore
+      .collection("propertiesToRent")
+      .doc(property.id)
+      .set(property);
+    res.status(201).json(property);
   } catch (error) {
-    res.status(404).send(error.message);
+    res.status(400).send(error.message);
   }
 };
 
@@ -94,7 +71,6 @@ const getPropertyByUserId = async (req, res, next) => {
       propertySnapshot.forEach((doc) => {
         propertyByUserId.push({ ...doc.data(), id: doc.id });
       });
-      console.log("property from db:", propertyByUserId);
       res.send(propertyByUserId);
     }
   } catch (error) {
@@ -119,56 +95,120 @@ const getPropertyToRent = async (req, res) => {
   }
 };
 
-const getPropertyByCountryCity = async (req, res) => {
-  try {
-    const { country, city } = req.query;
-    console.log(country, city);
-    const propertiesCollection = firestore.collection("propertiesToRent");
-    const propertySnapshot = await propertiesCollection
-      .where("location.country", "==", country)
-      .where("location.city", "==", city)
-      .get();
+const searchProperties = async (req, res) => {
+  const { availableFrom, availableTo, country } = req.query;
 
-    if (propertySnapshot.empty) {
-      res.status(404).send("Property with the given data not found !");
-    } else {
-      let property = [];
-      propertySnapshot.forEach((doc) => {
-        property.push(doc.data());
-      });
-      console.log("property from db:", property);
-      res.send(property);
+  if (!availableFrom && !availableTo && !country) {
+    try {
+      const propertiesToRent = await firestore.collection("propertiesToRent");
+      const data = await propertiesToRent.get();
+      const propertiesToRentArray = [];
+      if (data.empty) {
+        res.status(404).send("No property record found");
+      } else {
+        data.forEach((doc) => {
+          const propertyToRent = new PropertyToRent(
+            doc.id,
+            doc.data().userId,
+            doc.data().rating,
+            doc.data().placeType,
+            doc.data().propertyType,
+            doc.data().roomType,
+            doc.data().numberOfGuests,
+            doc.data().numberOfBedrooms,
+            doc.data().numberOfBeds,
+            doc.data().numberOfBathrooms,
+            doc.data().bathroomPrivate,
+            doc.data().location,
+            doc.data().amenities,
+            doc.data().photos
+          );
+          propertiesToRentArray.push(propertyToRent);
+        });
+        res.send(propertiesToRentArray);
+      }
+    } catch (error) {
+      res.status(400).send(error.message);
     }
-  } catch (error) {
-    res.status(404).send(error.message);
+  }
+
+  if (!availableFrom && !availableTo && country) {
+    try {
+      const propertiesCollection = firestore.collection("propertiesToRent");
+      const propertySnapshot = await propertiesCollection
+        .where("location.country", "==", country)
+        .get();
+
+      if (propertySnapshot.empty) {
+        res.status(404).send("Property with the given country not found !");
+      } else {
+        let propertyByCountry = [];
+        propertySnapshot.forEach((doc) => {
+          propertyByCountry.push({ ...doc.data(), id: doc.id });
+        });
+        res.send(propertyByCountry);
+      }
+    } catch (error) {
+      res.status(404).send(error.message);
+    }
+  }
+
+  if (availableFrom && availableTo && !country) {
+    try {
+      const propertiesCollection = firestore.collection("propertiesToRent");
+      const propertySnapshot = await propertiesCollection.get();
+      if (propertySnapshot.empty) {
+        res.status(404).send("Property with the given dates not found !");
+      } else {
+        let propertyByDates = [];
+        propertySnapshot.forEach((doc) => {
+          propertyByDates.push(doc.data());
+        });
+        res.send(
+          propertyByDates.filter(
+            (property) =>
+              moment(property.location.availableFrom).isSameOrBefore(
+                availableFrom
+              ) &&
+              moment(property.location.availableTo).isSameOrAfter(availableTo)
+          )
+        );
+      }
+    } catch (error) {
+      res.status(404).send(error.message);
+    }
+  }
+
+  if (availableFrom && availableTo && country) {
+    try {
+      const propertiesCollection = firestore.collection("propertiesToRent");
+      const propertySnapshot = await propertiesCollection
+        .where("location.country", "==", country)
+        .get();
+      if (propertySnapshot.empty) {
+        res.status(404).send("Property with the given dates not found !");
+      } else {
+        let propertyByDates = [];
+        propertySnapshot.forEach((doc) => {
+          propertyByDates.push(doc.data());
+        });
+        res.send(
+          propertyByDates.filter(
+            (property) =>
+              (moment(property.location.availableFrom).isBefore(
+                availableFrom
+              ) &&
+                moment(property.location.availableTo).isAfter(availableTo)) ||
+              moment(property.location.availableFrom).isSame(availableFrom) ||
+              moment(property.location.availableTo).isSame(availableTo)
+          )
+        );
+      }
+    } catch (error) {
+      res.status(404).send(error.message);
+    }
   }
 };
-
-const getPropertyAvailable = async (req, res) => {
-  console.log(req.query);
-};
-//   try {
-//     const { availableFrom, availableTo } = req.query;
-//     const propertiesCollection = firestore.collection("propertiesToRent");
-//     const propertySnapshot = await propertiesCollection
-//       .where("location.availableFrom", ">=", availableFrom)
-//       .where("location.availableTo", "<=", availableTo)
-//       .get();
-
-//     if (propertySnapshot.empty) {
-//       res.status(404).send("Property with the given dates not found !");
-//     } else {
-//       let propertyByDates = [];
-//       propertySnapshot.forEach((doc) => {
-//         propertyByDates.push(doc.data());
-//       });
-//       console.log("property from db:", propertyByDates);
-//       res.send(propertyByDates);
-//     }
-//   } catch (error) {
-//     res.status(404).send(error.message);
-//   }
-// };
 
 const updatePropertyToRent = async (req, res, next) => {
   try {
@@ -195,13 +235,11 @@ const deletePropertyToRent = async (req, res, next) => {
 };
 
 module.exports = {
-  addPropertyToRent,
   getAllPropertiesToRent,
+  addPropertyToRent,
   getPropertyToRent,
-  getPropertyByCountryCity,
   updatePropertyToRent,
   deletePropertyToRent,
-  getPropertyAvailable,
-  getPropertyByCountry,
   getPropertyByUserId,
+  searchProperties,
 };
--- routes/propertyToRent-routes.js
+++ routes/propertyToRent-routes.js
@@ -3,22 +3,18 @@ const {
   addPropertyToRent,
   getAllPropertiesToRent,
   getPropertyToRent,
-  getPropertyByCountryCity,
   updatePropertyToRent,
   deletePropertyToRent,
-  getPropertyAvailable,
-  getPropertyByCountry,
   getPropertyByUserId,
+  searchProperties,
 } = require("../controllers/propertyToRentController");
 
 const router = express.Router();
 
 router.post("/propertyToRent", addPropertyToRent);
 router.get("/propertiesToRent", getAllPropertiesToRent);
-router.get("/properties", getPropertyByCountryCity);
-router.get("/country/:country", getPropertyByCountry);
 router.get("/properties/:userId", getPropertyByUserId);
-router.get("/available", getPropertyAvailable);
+router.get("/available", searchProperties);
 router.get("/propertiesToRent/:id", getPropertyToRent);
 router.put("/propertiesToRent/:id", updatePropertyToRent);
 router.delete("/propertiesToRent/:id", deletePropertyToRent);
